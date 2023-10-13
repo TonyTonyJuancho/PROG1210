f5d5eec6098c66e643d44b14d4b0e0793b2851e7
Philip Soriano <philip.soriano@hotmail.com>
2023-10-13 16:28:39 -0400

    added tab for dropdown

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    console.log("Hello there!!");
+    document.querySelector(".dropdown-link").addEventListener("focus", (event)=>{
+        let dropdownLink = event.target;
+        dropdownLink.classList.add("tabbed")
+    })
+    document.addEventListener("focus", ()=>{
+        let dropdown = document.querySelector(".dropdown-link")
+        if(!document.activeElement.classList.contains("lookup-link"))
+        {
+            dropdown.classList.remove("tabbed")
+        }
+    })
 })
