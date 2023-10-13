277da5c4dd9ae161eccf1a972d43938839216208
Philip Soriano <philip.soriano@hotmail.com>
2023-10-13 17:19:18 -0400

    fixed styles and tab/active for dropdown

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,7 +3,26 @@ document.addEventListener('DOMContentLoaded',()=>{
         let dropdownLink = event.target;
         dropdownLink.classList.add("tabbed")
     })
+   document.addEventListener("click", (event)=>{
+        let item = event.target;
+        let dropdown = document.querySelector(".dropdown-link")
+        if(item.classList.contains("dropdown-link"))
+        {
+            dropdown.classList.add("tabbed")
+        }
+        else
+        {
+            dropdown.classList.remove("tabbed")
+        }
+    })
     document.addEventListener("focus", ()=>{
+        let dropdown = document.querySelector(".dropdown-link")
+        if(!document.activeElement.classList.contains("lookup-link"))
+        {
+            dropdown.classList.remove("tabbed")
+        }
+    })
+    document.addEventListener("blur", ()=>{
         let dropdown = document.querySelector(".dropdown-link")
         if(!document.activeElement.classList.contains("lookup-link"))
         {
